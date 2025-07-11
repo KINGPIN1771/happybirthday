a8ef09f89187a32b2c45303a7f8dfeadaf520f3a
Antony Justus <106657716+KINGPIN1771@users.noreply.github.com>
2025-07-11 22:14:54 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-}
+  assetPrefix: '/happybirthday/',
+  basePath: '/happybirthday',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
